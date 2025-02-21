8e2580692e24c026b51f9edd422a788ba6c3d93c
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-21 15:32:59 +0300

    Create initAnimation fun.

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,10 +1,84 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_image_assets.dart';
+import 'package:evoluton_x/features/splash/presentation/widgets/custom_slide_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<Offset> positionAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initAnimation();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: DecoratedBox(
+        decoration: getBoxDecoration(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                AppImageAssets.logo,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              CustomSlideText(positionAnimation: positionAnimation),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return const BoxDecoration(
+      gradient: LinearGradient(
+        colors: [
+          AppColors.deepSkyBlueColor,
+          AppColors.primaryColor,
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      ),
+    );
+  }
+
+  void initAnimation() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 2,
+      ),
+    );
+    positionAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: const Offset(0, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
