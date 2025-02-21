9dcd2539b613b10c857d7d524040752063597a5d
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-21 11:11:11 +0300

    Create AppImageAssets && Add images to pubspec.yaml.

---
+++ lib/core/utils/app_image_assets.dart
@@ -0,0 +1,6 @@
+abstract class AppImageAssets {
+  static const String logo = 'assets/images/logo.svg';
+  static const String onboarding1 = 'assets/images/onboarding_1.svg';
+  static const String onboarding2 = 'assets/images/onboarding_2.svg';
+  static const String onboarding3 = 'assets/images/onboarding_3.svg';
+}
