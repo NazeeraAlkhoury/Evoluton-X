1f9962be944cef391195c8b7d6e319f84a565996
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-21 13:56:20 +0300

    Update AppAssetsImage.

--- lib/core/utils/app_image_assets.dart
+++ lib/core/utils/app_image_assets.dart
@@ -1,5 +1,5 @@
 abstract class AppImageAssets {
-  static const String logo = 'assets/images/logo.svg';
+  static const String logo = 'assets/images/logo.png';
   static const String onboarding1 = 'assets/images/onboarding_1.svg';
   static const String onboarding2 = 'assets/images/onboarding_2.svg';
   static const String onboarding3 = 'assets/images/onboarding_3.svg';
