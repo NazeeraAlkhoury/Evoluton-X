d20620ab3b6f54e19d556242788224860e9a8ad2
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-22 15:30:57 +0300

    Create Authentication feature.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:evoluton_x/core/utils/app_themes.dart';
-import 'package:evoluton_x/features/onboarding/presentation/views/onboarding_view.dart';
+import 'package:evoluton_x/features/authentication/presentation/views/register_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // DevicePreview(
+    //   builder: (context) => const MyApp(),
+    // ),
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +21,9 @@ class MyApp extends StatelessWidget {
       title: 'Evoluton-X',
       theme: AppThemes.lightThemeData,
       debugShowCheckedModeBanner: false,
-      home: const OnBoardingView(),
+      home: const RegisterView(),
+      // locale: DevicePreview.locale(context),
+      // builder: DevicePreview.appBuilder,
     );
   }
 }
