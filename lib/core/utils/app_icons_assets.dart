4ceba11008d0d98046e0910b9585529d15520204
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-23 11:50:08 +0300

    Create AppIconsAssets && Add to pubspec.

---
+++ lib/core/utils/app_icons_assets.dart
@@ -0,0 +1,5 @@
+abstract class AppIconAssets {
+  static const String user = 'assets/icons/user.svg';
+  static const String mail = 'assets/icons/mail.svg';
+  static const String key = 'assets/icons/key.svg';
+}
