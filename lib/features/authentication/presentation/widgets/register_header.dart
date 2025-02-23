aeff2b074c4178034877932f3d37a0ee06c08259
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-23 13:10:34 +0300

    Create RegisterHeader.

---
+++ lib/features/authentication/presentation/widgets/register_header.dart
@@ -0,0 +1,26 @@
+import 'package:evoluton_x/core/utils/app_image_assets.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/features/authentication/presentation/widgets/custom_back_button.dart';
+import 'package:flutter/material.dart';
+
+class RegisterHeader extends StatelessWidget {
+  const RegisterHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const CustomBackButton(),
+        Image.asset(AppImageAssets.logo),
+        Text(
+          AppStrings.register,
+          style: AppTextStyles.styleSemiBold20(context),
+        ),
+      ],
+    );
+  }
+}
