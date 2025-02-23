02569538066be891f04390b4f1860f567b3af8f9
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-23 14:28:21 +0300

    Add visibiltu to AppIconAssets.

--- lib/core/utils/app_icons_assets.dart
+++ lib/core/utils/app_icons_assets.dart
@@ -2,4 +2,6 @@ abstract class AppIconAssets {
   static const String user = 'assets/icons/user.svg';
   static const String mail = 'assets/icons/mail.svg';
   static const String key = 'assets/icons/key.svg';
+  static const String visibility = 'assets/icons/visibility.svg';
+  static const String visibilityOff = 'assets/icons/visibility_off.svg';
 }
