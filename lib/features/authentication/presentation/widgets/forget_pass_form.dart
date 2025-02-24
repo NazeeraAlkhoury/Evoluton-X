9678fa388e55b2d0822b184c460f7babce630b29
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-24 21:12:33 +0300

    Create ForgetPassForm.

---
+++ lib/features/authentication/presentation/widgets/forget_pass_form.dart
@@ -0,0 +1,47 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_icons_assets.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/core/widgets/app_button.dart';
+import 'package:evoluton_x/features/authentication/presentation/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPassForm extends StatelessWidget {
+  const ForgetPassForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            AppStrings.enterEmailForConfirmation,
+            style: AppTextStyles.styleSemiBold16(context),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            AppStrings.email,
+            style: AppTextStyles.styleSemiBold16(context)
+                .copyWith(color: AppColors.darkGreyColor),
+          ),
+          const SizedBox(height: 8),
+          const CustomTextFormField(
+            hintText: AppStrings.enterEmail,
+            prefixIcon: AppIconAssets.mail,
+          ),
+          const SizedBox(height: 16),
+          const SizedBox(height: 60),
+          AppButton(
+            textButton: AppStrings.login,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
