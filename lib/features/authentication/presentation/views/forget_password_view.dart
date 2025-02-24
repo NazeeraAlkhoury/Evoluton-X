365384cc92b4ea0d4acbdfdddc90d05da7ff28bb
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-24 21:13:41 +0300

    Refactoring code.

---
+++ lib/features/authentication/presentation/views/forget_password_view.dart
@@ -0,0 +1,13 @@
+import 'package:evoluton_x/features/authentication/presentation/widgets/forget_pass_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPasswordView extends StatelessWidget {
+  const ForgetPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ForgetPassViewBody(),
+    );
+  }
+}
--- lib/features/authentication/presentation/widgets/forget_pass_view_body.dart
+++ lib/features/authentication/presentation/widgets/forget_pass_view_body.dart
@@ -1,11 +1,3 @@
-import 'package:evoluton_x/core/utils/app_colors.dart';
-import 'package:evoluton_x/core/utils/app_icons_assets.dart';
-import 'package:evoluton_x/core/utils/app_strings.dart';
-import 'package:evoluton_x/core/utils/app_text_styles.dart';
-import 'package:evoluton_x/core/widgets/app_button.dart';
-import 'package:evoluton_x/features/authentication/presentation/widgets/auth_header_row.dart';
-import 'package:evoluton_x/features/authentication/presentation/widgets/custom_back_button.dart';
-import 'package:evoluton_x/features/authentication/presentation/widgets/custom_text_form_field.dart';
 import 'package:evoluton_x/features/authentication/presentation/widgets/forget_pass_form.dart';
 import 'package:evoluton_x/features/authentication/presentation/widgets/forget_pass_header.dart';
 import 'package:flutter/material.dart';
