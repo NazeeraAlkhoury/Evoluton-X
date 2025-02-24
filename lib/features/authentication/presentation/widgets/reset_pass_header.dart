14bbc62e43f2794e5ce69774068eeace6d0cf2fa
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-24 21:26:06 +0300

    Create ResetPassHeader.

---
+++ lib/features/authentication/presentation/widgets/reset_pass_header.dart
@@ -0,0 +1,25 @@
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/features/authentication/presentation/widgets/auth_header_row.dart';
+import 'package:evoluton_x/features/authentication/presentation/widgets/custom_back_button.dart';
+import 'package:flutter/material.dart';
+
+class ResetPassHeader extends StatelessWidget {
+  const ResetPassHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomBackButton(),
+        SizedBox(
+          height: 12,
+        ),
+        AuthHeaderRow(
+          title: AppStrings.resetPassword,
+        ),
+      ],
+    );
+  }
+}
