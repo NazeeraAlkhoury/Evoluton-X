271f32ed74e6025d116567406cc1b7c2d5b098d0
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-24 16:45:46 +0300

    Create VerifyPasswordView.

---
+++ lib/features/authentication/presentation/views/verify_password_view.dart
@@ -0,0 +1,13 @@
+import 'package:evoluton_x/features/authentication/presentation/widgets/verify_password_view_body.dart';
+import 'package:flutter/material.dart';
+
+class VerifyPasswordView extends StatelessWidget {
+  const VerifyPasswordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: VerifyPasswordViewBody(),
+    );
+  }
+}
---
+++ lib/features/authentication/presentation/widgets/verify_password_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VerifyPasswordViewBody extends StatelessWidget {
+  const VerifyPasswordViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
