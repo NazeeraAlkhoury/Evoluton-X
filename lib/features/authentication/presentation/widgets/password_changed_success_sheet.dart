c09b4469911da52d519601ea7d005d8bb91c16ba
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-25 13:27:18 +0300

    Create PasswordChangedSuccessSheet.

---
+++ lib/features/authentication/presentation/widgets/password_changed_success_sheet.dart
@@ -0,0 +1,41 @@
+import 'package:evoluton_x/core/utils/app_image_assets.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/core/widgets/app_button.dart';
+import 'package:evoluton_x/core/widgets/custom_bottomsheet_handle.dart';
+import 'package:flutter/material.dart';
+
+class PasswordChangedSuccessSheet extends StatelessWidget {
+  const PasswordChangedSuccessSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const CustomBottomSheetHandle(),
+        const SizedBox(
+          height: 30,
+        ),
+        Image.asset(AppImageAssets.changePassSuccess),
+        const SizedBox(
+          height: 60,
+        ),
+        Text(
+          AppStrings.passwordChangedSuccessfully,
+          textAlign: TextAlign.center,
+          style: AppTextStyles.styleSemiBold18(context),
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        AppButton(
+          textButton: AppStrings.loginNow,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
