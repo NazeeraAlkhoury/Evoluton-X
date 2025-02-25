c8348fd00b167816c5f79ea007445826882764c2
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-25 12:44:02 +0300

    add changePassSuccess image to AppImageAssets.

--- lib/core/utils/app_image_assets.dart
+++ lib/core/utils/app_image_assets.dart
@@ -3,4 +3,6 @@ abstract class AppImageAssets {
   static const String onboarding1 = 'assets/images/onboarding_1.png';
   static const String onboarding2 = 'assets/images/onboarding_2.png';
   static const String onboarding3 = 'assets/images/onboarding_3.png';
+  static const String changePassSuccess =
+      'assets/images/change_pass_successfully.png';
 }
