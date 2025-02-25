598a773d094007290c6b7881b602962d59c71d2c
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-25 13:26:26 +0300

    Create showCustomBottomSheet function.

---
+++ lib/core/functions/show_custom_bottom_sheet.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> showCustomBottomSheet(
+  BuildContext context, {
+  required Widget child,
+}) {
+  return showModalBottomSheet(
+    context: context,
+    elevation: 0,
+    isScrollControlled: true,
+    isDismissible: false,
+    barrierColor: Colors.black.withOpacity(0.8),
+    builder: (context) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 8, left: 20, right: 20, bottom: 30),
+        child: child,
+      );
+    },
+  );
+}
