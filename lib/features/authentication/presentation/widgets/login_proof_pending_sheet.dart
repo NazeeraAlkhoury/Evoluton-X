910dfa58d37c95b4e3390b7bc7e46627de01f75c
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-26 22:52:36 +0300

    Create LoinProofPendingSheet.

---
+++ lib/features/authentication/presentation/widgets/login_proof_pending_sheet.dart
@@ -0,0 +1,46 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/widgets/app_button.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/app_text_styles.dart';
+
+class LoginProofPendingSheet extends StatelessWidget {
+  const LoginProofPendingSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            AppStrings.loginPending,
+            textAlign: TextAlign.center,
+            style: AppTextStyles.styleSemiBold18(context)
+                .copyWith(color: AppColors.blackColor),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            AppStrings.documentReviewEmail,
+            textAlign: TextAlign.center,
+            style: AppTextStyles.styleRegular14(context),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          AppButton(
+            textButton: AppStrings.returnHome,
+            onPressed: () {},
+            widthButton: double.infinity,
+          ),
+        ],
+      ),
+    );
+  }
+}
