a455a3b365a87b85c4ab70348803aa0b7431afc3
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-26 14:56:48 +0300

    Add Icon more to pubSpec.

--- lib/core/utils/app_icons_assets.dart
+++ lib/core/utils/app_icons_assets.dart
@@ -4,4 +4,5 @@ abstract class AppIconAssets {
   static const String key = 'assets/icons/key.svg';
   static const String visibility = 'assets/icons/visibility.svg';
   static const String visibilityOff = 'assets/icons/visibility_off.svg';
+  static const String more = 'assets/icons/more.svg';
 }
