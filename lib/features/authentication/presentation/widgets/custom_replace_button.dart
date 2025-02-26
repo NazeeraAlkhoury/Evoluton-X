878e0bfbbad8a0c8a88ff562b85b02ee081aaa5a
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-26 22:10:47 +0300

    Create CustomReplaceButton.

---
+++ lib/features/authentication/presentation/widgets/custom_replace_button.dart
@@ -0,0 +1,37 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomReplaceButton extends StatelessWidget {
+  const CustomReplaceButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 44,
+      width: 118,
+      child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+              side: const BorderSide(
+                color: AppColors.primaryColor,
+                width: 1.5,
+              ),
+            ),
+            elevation: 0,
+          ),
+          child: Text(
+            AppStrings.replace,
+            style: AppTextStyles.styleSemiBold16(context).copyWith(
+              color: AppColors.primaryColor,
+            ),
+          )),
+    );
+  }
+}
