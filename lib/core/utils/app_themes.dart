3fc763710655cee757258021b723616959f4b68b
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-02-28 14:43:45 +0300

    Add bottomNavigationBarTheme to AppTheme.

--- lib/core/utils/app_themes.dart
+++ lib/core/utils/app_themes.dart
@@ -13,5 +13,14 @@ abstract class AppThemes {
     colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
     useMaterial3: true,
     textTheme: GoogleFonts.poppinsTextTheme(),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: AppColors.primaryColor,
+      selectedItemColor: AppColors.whiteColor,
+      unselectedItemColor: AppColors.blackColor,
+      selectedLabelStyle: TextStyle(fontSize: 0),
+      unselectedLabelStyle: TextStyle(fontSize: 0),
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+    ),
   );
 }
