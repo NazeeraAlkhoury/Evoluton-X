740ff9076dbbb3105210cd5760c1d198ab170afa
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-04 16:27:52 +0300

    Create ClubSuggestionsGrid.

---
+++ lib/features/details/presentation/widgets/filter_clubs/club_suggestion_grid.dart
@@ -0,0 +1,22 @@
+import 'package:evoluton_x/features/details/presentation/widgets/custom_club.dart';
+import 'package:flutter/material.dart';
+
+class ClubSuggestionsGrid extends StatelessWidget {
+  const ClubSuggestionsGrid({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.builder(
+      itemCount: 10,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 1.1 / 1.03,
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 6,
+      ),
+      itemBuilder: (context, index) => const CustomClub(),
+    );
+  }
+}
