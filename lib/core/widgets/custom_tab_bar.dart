1eca877b07953ba2f4e462a35277124bf743f992
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-06 11:53:43 +0300

    Create CustomTabBarView.

---
+++ lib/core/widgets/custom_tab_bar.dart
@@ -0,0 +1,50 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class CustomTabBar extends StatelessWidget {
+  final String firstTab;
+  final String secondTab;
+  final TabController tabController;
+  const CustomTabBar({
+    super.key,
+    required this.tabController,
+    required this.firstTab,
+    required this.secondTab,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      margin: const EdgeInsets.symmetric(horizontal: 50),
+      decoration: BoxDecoration(
+        color: AppColors.darkGreyColor,
+        borderRadius: BorderRadius.circular(
+          30,
+        ),
+      ),
+      child: TabBar(
+        controller: tabController,
+        indicator: BoxDecoration(
+            color: AppColors.primaryColor,
+            borderRadius: BorderRadius.circular(30)),
+        indicatorColor: AppColors.primaryColor,
+        indicatorSize: TabBarIndicatorSize.tab,
+        labelColor: AppColors.offWhiteColor,
+        dividerColor: Colors.transparent,
+        unselectedLabelColor: const Color(0xffD4D2D2),
+        labelStyle: AppTextStyles.styleSemiBold18(context),
+        unselectedLabelStyle: AppTextStyles.styleSemiBold18(context),
+        tabs: [
+          Tab(
+            text: firstTab,
+          ),
+          Tab(
+            text: secondTab,
+          ),
+        ],
+      ),
+    );
+  }
+}
