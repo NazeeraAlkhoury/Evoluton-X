efe1e2776b404a4aab62ac5180be1fd2923533e8
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-06 13:21:23 +0300

    Create  StatisticTabBarView.

---
+++ lib/features/details/presentation/widgets/player_card/statistic_tab_bar_view.dart
@@ -0,0 +1,80 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/core/widgets/app_button.dart';
+import 'package:evoluton_x/features/details/presentation/widgets/player_card/statistic_row.dart';
+import 'package:flutter/material.dart';
+
+class StatisticTabBarView extends StatelessWidget {
+  const StatisticTabBarView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> statisticNames = [
+      AppStrings.finshing,
+      AppStrings.crossing,
+      AppStrings.headingAccuracy,
+      AppStrings.shortPassing,
+      AppStrings.volleys,
+      AppStrings.dribbling,
+      AppStrings.curve,
+      AppStrings.freekickAccuracy,
+      AppStrings.ballControl,
+      AppStrings.acceleration,
+      AppStrings.sprinSpeed,
+      AppStrings.shotPower,
+      AppStrings.marking,
+      AppStrings.strength,
+    ];
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: StatisticRow(
+                name: statisticNames[index],
+                value: '85/100',
+              ),
+            ),
+            itemCount: statisticNames.length,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            AppStrings.tryModel,
+            style: AppTextStyles.styleSemiBold18(context)
+                .copyWith(color: AppColors.blackColor),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            AppStrings.tryModelDesciption,
+            style: AppTextStyles.styleRegular14(context),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          AppButton(
+            textButton: AppStrings.starScaning,
+            onPressed: () {},
+            widthButton: double.infinity,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+        ],
+      ),
+    );
+  }
+}
