8fef7b26c5013acb45dcbd43cfa18117d67f5cba
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-06 13:20:58 +0300

    Create StatisticRow.

---
+++ lib/features/details/presentation/widgets/player_card/statistic_row.dart
@@ -0,0 +1,30 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:flutter/material.dart';
+
+class StatisticRow extends StatelessWidget {
+  final String name;
+  final String value;
+  const StatisticRow({
+    super.key,
+    required this.name,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          name,
+          style: AppTextStyles.styleSemiBold18(context)
+              .copyWith(color: AppColors.blackColor),
+        ),
+        Text(
+          value,
+          style: AppTextStyles.styleRegular16(context),
+        ),
+      ],
+    );
+  }
+}
