d1c31e0a38fcd7b12813c319aa3ec5e98716d9a0
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-09 16:02:20 +0300

    ClubCardView is Done.

--- lib/features/details/presentation/views/club_card_view.dart
+++ lib/features/details/presentation/views/club_card_view.dart
@@ -1,20 +1,4 @@
-import 'package:evoluton_x/core/utils/app_colors.dart';
-import 'package:evoluton_x/core/utils/app_strings.dart';
-import 'package:evoluton_x/core/utils/app_text_styles.dart';
-import 'package:evoluton_x/core/widgets/custom_show_row.dart';
-import 'package:evoluton_x/core/widgets/custom_tab_bar.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/achieved_chances_section.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/assists_section.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_card_club_sliver_appbar.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_goal_states.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_player_profile_row.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_players_xg.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/goales_section.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/team_tab_bar_view.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/top_midfield_section.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/top_strikers_section.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/xg_section.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/player_card/sliver_appbar_delegate.dart';
+import 'package:evoluton_x/features/details/presentation/widgets/club_card/club_card_view_body.dart';
 import 'package:flutter/material.dart';
 
 class ClubCardView extends StatefulWidget {
@@ -43,75 +27,7 @@ class _ClubCardViewState extends State<ClubCardView>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.white,
-      body: SafeArea(
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) => [
-            const CustomCardClubSleverAppBar(),
-            const SliverToBoxAdapter(
-              child: SizedBox(height: 30),
-            ),
-            SliverPersistentHeader(
-              delegate: SliverAppBarDelegate(
-                CustomTabBar(
-                  tabController: _tabController,
-                  firstTab: AppStrings.team,
-                  secondTab: AppStrings.topPlayers,
-                ),
-              ),
-            ),
-          ],
-          body: TabBarView(
-            controller: _tabController,
-            children: const [TeamTabBarView(), TopPlayersTaoBarView()],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TopPlayersTaoBarView extends StatelessWidget {
-  const TopPlayersTaoBarView({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: [
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TopStrikersSection(),
-                SizedBox(
-                  height: 20,
-                ),
-                TopMidfielderSection(),
-                SizedBox(
-                  height: 20,
-                ),
-                GoalsSection(),
-                SizedBox(
-                  height: 20,
-                ),
-                AssistsSection(),
-                SizedBox(
-                  height: 20,
-                ),
-                XgSection(),
-                SizedBox(
-                  height: 20,
-                ),
-                AchievedChancesSection(),
-              ],
-            ),
-          ),
-        ),
-      ],
+      body: ClubCardViewBody(tabController: _tabController),
     );
   }
 }
