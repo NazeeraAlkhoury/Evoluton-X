5ca7f6a39b9cd4fc87f5dd7694a80c0cf150b482
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-09 13:44:01 +0300

    Create TopStrikersSection.

--- lib/features/details/presentation/views/club_card_view.dart
+++ lib/features/details/presentation/views/club_card_view.dart
@@ -1,6 +1,9 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
 import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
 import 'package:evoluton_x/core/widgets/custom_tab_bar.dart';
 import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_card_club_sliver_appbar.dart';
+import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_player_profile_row.dart';
 import 'package:evoluton_x/features/details/presentation/widgets/club_card/team_tab_bar_view.dart';
 import 'package:evoluton_x/features/details/presentation/widgets/player_card/sliver_appbar_delegate.dart';
 import 'package:flutter/material.dart';
@@ -53,9 +56,67 @@ class _ClubCardViewState extends State<ClubCardView>
             controller: _tabController,
             children: [
               const TeamTabBarView(),
-              Container(
-                color: Colors.red,
-              ),
+              CustomScrollView(
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                AppStrings.topStrikers,
+                                style: AppTextStyles.styleSemiBold18(context)
+                                    .copyWith(color: AppColors.blackColor),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemBuilder: (context, index) => const Padding(
+                                  padding: EdgeInsets.only(bottom: 10),
+                                  child: CustomPlayerProfileRow(),
+                                ),
+                                itemCount: 4,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                AppStrings.topMidfielder,
+                                style: AppTextStyles.styleSemiBold18(context)
+                                    .copyWith(color: AppColors.blackColor),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemBuilder: (context, index) => const Padding(
+                                  padding: EdgeInsets.only(bottom: 10),
+                                  child: CustomPlayerProfileRow(),
+                                ),
+                                itemCount: 4,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
