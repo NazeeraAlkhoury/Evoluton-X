1dbcf70ce0c3fa9981625ffc0675e1bbacb1e337
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-09 13:32:02 +0300

    Create CoachSection.

---
+++ lib/features/details/presentation/widgets/club_card/coach_section.dart
@@ -0,0 +1,29 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_player_profile_row.dart';
+import 'package:flutter/material.dart';
+
+class CoachSection extends StatelessWidget {
+  const CoachSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          AppStrings.coach,
+          style: AppTextStyles.styleSemiBold18(context)
+              .copyWith(color: AppColors.blackColor),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const CustomPlayerProfileRow(),
+      ],
+    );
+  }
+}
