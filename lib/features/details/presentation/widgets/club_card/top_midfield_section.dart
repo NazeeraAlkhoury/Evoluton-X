1d1d59d14621043fa24f594c62c6d8005b9b1866
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-09 13:44:19 +0300

    Creat TopMidfielderSection.

---
+++ lib/features/details/presentation/widgets/club_card/top_midfield_section.dart
@@ -0,0 +1,48 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_strings.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_player_profile_row.dart';
+import 'package:flutter/material.dart';
+
+class TopMidfielderSection extends StatelessWidget {
+  const TopMidfielderSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          AppStrings.topMidfielder,
+          style: AppTextStyles.styleSemiBold18(context)
+              .copyWith(color: AppColors.blackColor),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const TopMidfielderListView(),
+      ],
+    );
+  }
+}
+
+class TopMidfielderListView extends StatelessWidget {
+  const TopMidfielderListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) => const Padding(
+        padding: EdgeInsets.only(bottom: 10),
+        child: CustomPlayerProfileRow(),
+      ),
+      itemCount: 4,
+    );
+  }
+}
