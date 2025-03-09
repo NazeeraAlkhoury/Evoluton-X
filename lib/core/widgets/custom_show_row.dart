82e5e497097076f75ab751a96cdcc343972792fc
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-09 14:12:25 +0300

    Create CustomShowRow.

--- lib/core/widgets/custom_show_row.dart
+++ lib/core/widgets/custom_show_row.dart
@@ -1,13 +1,15 @@
 import 'package:evoluton_x/core/utils/app_colors.dart';
 import 'package:evoluton_x/core/utils/app_strings.dart';
 import 'package:evoluton_x/core/utils/app_text_styles.dart';
-import 'package:evoluton_x/features/details/presentation/views/filter_clubs_view.dart';
 import 'package:flutter/material.dart';
-import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
-class BestClubsShowRow extends StatelessWidget {
-  const BestClubsShowRow({
+class CustomShowRow extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+  const CustomShowRow({
     super.key,
+    required this.text,
+    this.onPressed,
   });
 
   @override
@@ -16,19 +18,12 @@ class BestClubsShowRow extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Text(
-          AppStrings.bestclubs,
+          text,
           style: AppTextStyles.styleSemiBold18(context)
               .copyWith(color: AppColors.blackColor),
         ),
         TextButton(
-          onPressed: () {
-            PersistentNavBarNavigator.pushNewScreen(
-              context,
-              screen: const FilterClubsView(),
-              withNavBar: true,
-              pageTransitionAnimation: PageTransitionAnimation.cupertino,
-            );
-          },
+          onPressed: onPressed,
           child: Text(
             AppStrings.showAll,
             style: AppTextStyles.styleRegular14(context),
