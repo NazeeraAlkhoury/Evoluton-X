c0a63cbb14fed8a59c18eae9ee1744e18f8fb5c6
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-09 13:32:54 +0300

    TeamTabBarView is Done.

--- lib/features/details/presentation/views/club_card_view.dart
+++ lib/features/details/presentation/views/club_card_view.dart
@@ -1,10 +1,7 @@
-import 'package:evoluton_x/core/utils/app_colors.dart';
 import 'package:evoluton_x/core/utils/app_strings.dart';
-import 'package:evoluton_x/core/utils/app_text_styles.dart';
-import 'package:evoluton_x/core/widgets/custom_flag_or_club_avatar.dart';
 import 'package:evoluton_x/core/widgets/custom_tab_bar.dart';
 import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_card_club_sliver_appbar.dart';
-import 'package:evoluton_x/features/details/presentation/widgets/club_card/custom_player_profile_row.dart';
+import 'package:evoluton_x/features/details/presentation/widgets/club_card/team_tab_bar_view.dart';
 import 'package:evoluton_x/features/details/presentation/widgets/player_card/sliver_appbar_delegate.dart';
 import 'package:flutter/material.dart';
 
@@ -55,23 +52,7 @@ class _ClubCardViewState extends State<ClubCardView>
           body: TabBarView(
             controller: _tabController,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      AppStrings.coach,
-                      style: AppTextStyles.styleSemiBold18(context)
-                          .copyWith(color: AppColors.blackColor),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const CustomPlayerProfileRow(),
-                  ],
-                ),
-              ),
+              const TeamTabBarView(),
               Container(
                 color: Colors.red,
               ),
