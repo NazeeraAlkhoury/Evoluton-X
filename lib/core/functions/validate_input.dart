323716b46f63489f19485d08975f761557567631
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-15 18:15:01 +0300

    Add new validation types to validateInput function.

--- lib/core/functions/validate_input.dart
+++ lib/core/functions/validate_input.dart
@@ -1,17 +1,18 @@
 import 'package:evoluton_x/core/utils/app_strings.dart';
 
-String? validateInput(
-  String val,
-  int min,
+String? validateInput({
+  required String val,
+  int? min,
   int? max,
-  String type,
-) {
+  required String type,
+  bool? repeatPassword,
+}) {
   if (type == 'password') {
     if (val.isEmpty) {
       return AppStrings.passwordNotEmpty;
     }
 
-    if (val.length < min) {
+    if (val.length < min!) {
       return AppStrings.minPassword;
     }
 
@@ -23,6 +24,34 @@ String? validateInput(
     bool isSymbol = !RegExp(r'(?=.*[\W_])').hasMatch(val);
     if (isUpperCase || isLowerCase || isSymbol) {
       return AppStrings.passwordTerm;
+    }
+  }
+
+  if (type == 'email') {
+    if (val.isEmpty) {
+      return AppStrings.emailNotEmpty;
+    }
+
+    final emailRegExp =
+        RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    if (!emailRegExp.hasMatch(val)) {
+      return AppStrings.notValidEmail;
+    }
+  }
+
+  if (type == "firstname" || type == "lastname") {
+    if (val.isEmpty) {
+      return '$type can\'t be empty';
+    }
+    final nameRegExp = RegExp(r'^[a-zA-Z ]{1,50}$');
+    if (!nameRegExp.hasMatch(val)) {
+      return "$type Not valid";
+    }
+  }
+
+  if (type == 'repeatPassword') {
+    if (!repeatPassword! || val.isEmpty) {
+      return AppStrings.passwordNotMatch;
     }
   }
   return null;
