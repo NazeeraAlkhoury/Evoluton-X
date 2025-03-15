e587201816fc881b7c6da3bda2f7b7e658815a04
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-15 18:11:24 +0300

    Add new features to CustomTextFormField.

--- lib/features/authentication/presentation/widgets/custom_text_form_field.dart
+++ lib/features/authentication/presentation/widgets/custom_text_form_field.dart
@@ -12,6 +12,8 @@ class CustomTextFormField extends StatelessWidget {
   final void Function()? onSuffix;
   final TextInputType? keyboardType;
   final String? Function(String?)? validator;
+  final void Function(String)? onFieldSubmitted;
+  final TextInputAction? textInputAction;
   const CustomTextFormField({
     super.key,
     this.controller,
@@ -22,47 +24,56 @@ class CustomTextFormField extends StatelessWidget {
     this.onSuffix,
     this.keyboardType,
     this.validator,
+    this.onFieldSubmitted,
+    this.textInputAction,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 44.53,
-      child: TextFormField(
-        controller: controller,
-        style: AppTextStyles.styleRegular14(context),
-        obscureText: isObscureText!,
-        obscuringCharacter: '*',
-        keyboardType: keyboardType,
-        validator: validator,
-        decoration: InputDecoration(
-          prefixIcon: Padding(
-            padding: const EdgeInsets.all(10),
-            child: SvgPicture.asset(prefixIcon),
-          ),
-          suffixIcon: suffixIcon != null
-              ? IconButton(
-                  onPressed: onSuffix,
-                  icon: SvgPicture.asset(suffixIcon!),
-                )
-              : null,
-          hintText: hintText,
-          hintStyle: AppTextStyles.styleRegular14(context)
-              .copyWith(color: AppColors.texthintColor),
-          filled: true,
-          fillColor: AppColors.fillFieldColor,
-          contentPadding: const EdgeInsets.symmetric(vertical: 10),
-          errorStyle: AppTextStyles.styleRegular12(context).copyWith(
-            color: AppColors.redColor,
-          ),
-          border: getBorderStyle(),
-          enabledBorder: getBorderStyle(),
-          focusedBorder: getBorderStyle(),
-          errorBorder: getBorderStyle(
-            borderSide: const BorderSide(color: AppColors.redColor, width: 1),
+    return TextFormField(
+      controller: controller,
+      style: AppTextStyles.styleRegular14(context),
+      obscureText: isObscureText!,
+      obscuringCharacter: '*',
+      keyboardType: keyboardType,
+      validator: validator,
+      decoration: InputDecoration(
+        prefixIcon: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SvgPicture.asset(
+            prefixIcon,
           ),
         ),
+        suffixIcon: suffixIcon != null
+            ? IconButton(
+                onPressed: onSuffix,
+                icon: SvgPicture.asset(suffixIcon!),
+              )
+            : null,
+        hintText: hintText,
+        hintStyle: AppTextStyles.styleRegular14(context)
+            .copyWith(color: AppColors.texthintColor),
+        filled: true,
+        fillColor: AppColors.fillFieldColor,
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        errorStyle: AppTextStyles.styleRegular12(context).copyWith(
+          color: AppColors.redColor,
+          overflow: TextOverflow.ellipsis,
+        ),
+        errorMaxLines: 2,
+        border: getBorderStyle(),
+        enabledBorder: getBorderStyle(),
+        focusedBorder: getBorderStyle(),
+        errorBorder: getBorderStyle(
+          borderSide: const BorderSide(color: AppColors.redColor, width: 1),
+        ),
+        focusedErrorBorder: getBorderStyle(
+          borderSide: const BorderSide(color: AppColors.redColor, width: 1),
+        ),
       ),
+      onFieldSubmitted: onFieldSubmitted,
+      textInputAction: textInputAction ?? TextInputAction.next,
     );
   }
 
