9163c9eb260b2a0092134e81c2f5b5f4f1448291
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-03-17 18:59:05 +0300

    Create CachServices.

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,3 +1,4 @@
+import 'package:evoluton_x/core/services/cach_services.dart';
 import 'package:evoluton_x/features/authentication/presentation/controllers/login_bloc/login_bloc.dart';
 import 'package:evoluton_x/features/authentication/presentation/controllers/password_bloc/password_bloc.dart';
 import 'package:evoluton_x/features/authentication/presentation/controllers/register_bloc/register_bloc.dart';
@@ -6,6 +7,7 @@ import 'package:evoluton_x/features/details/presentation/controllers/details_blo
 import 'package:evoluton_x/features/layout/presentation/controller/layout_bloc/layout_bloc.dart';
 import 'package:evoluton_x/features/search/presentation/search_bloc/search_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final GetIt getIt = GetIt.instance;
 
@@ -20,6 +22,13 @@ class ServiceLocator {
     // getIt.registerLazySingleton<ApiService>(
     //   () => ApiService(dio: getIt()),
     // );
+
+    //sharedPreferences
+    final sharedPreferences = await SharedPreferences.getInstance();
+    getIt.registerSingleton<SharedPreferences>(sharedPreferences);
+    getIt.registerLazySingleton<CachServices>(
+      () => CachServices(sharedPreferences: getIt<SharedPreferences>()),
+    );
 
     //Bloc
     getIt.registerFactory<RegisterBloc>(
