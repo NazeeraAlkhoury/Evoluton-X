0f1e6b61cc40a793b90e46e5b49c90c21828232b
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-01 21:28:17 +0300

    create PlayerDetailsCardModel.

---
+++ lib/features/details/domain/entities/player_details_card.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+class PlayerDetailsCard extends Equatable {
+  final String name;
+  final String pos;
+  final num power;
+  final num age;
+  final num overallRating;
+  final String preferredFoot;
+  final num valueEuro;
+  final List<String> imageUrl;
+  final String nation;
+  final String clubImage;
+
+  const PlayerDetailsCard({
+    required this.name,
+    required this.pos,
+    required this.power,
+    required this.age,
+    required this.overallRating,
+    required this.preferredFoot,
+    required this.valueEuro,
+    required this.imageUrl,
+    required this.nation,
+    required this.clubImage,
+  });
+
+  @override
+  List<Object?> get props => [
+        name,
+        pos,
+        power,
+        age,
+        overallRating,
+        valueEuro,
+        imageUrl,
+        nation,
+        clubImage,
+      ];
+}
