623da18d99565771f7c5a98009f81f4c21299d66
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-02 12:37:34 +0300

    create AllClubModel.

---
+++ lib/features/details/domain/entities/all_clubs.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:evoluton_x/features/details/domain/entities/club.dart';
+
+class AllClubs extends Equatable {
+  final String msg;
+  final List<Club> data;
+
+  const AllClubs({required this.msg, required this.data});
+
+  @override
+  List<Object?> get props => [
+        msg,
+        data,
+      ];
+}
