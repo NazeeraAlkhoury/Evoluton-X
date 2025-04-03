4ed3e69bef43d8e53353e585f7af396a4259fd05
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-03 15:45:39 +0300

    Integrate loginForm with BLoC, added BlocBuilder and BlocConsumer.

--- lib/features/authentication/presentation/widgets/login/login_form.dart
+++ lib/features/authentication/presentation/widgets/login/login_form.dart
@@ -1,9 +1,15 @@
+import 'package:evoluton_x/core/functions/show_custom_err_snack_bar.dart';
+import 'package:evoluton_x/core/functions/show_custom_success_snack_bar.dart';
 import 'package:evoluton_x/core/functions/validate_input.dart';
+import 'package:evoluton_x/core/services/cach_services.dart';
+import 'package:evoluton_x/core/services/service_locator.dart';
 import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_constants.dart';
 import 'package:evoluton_x/core/utils/app_icons_assets.dart';
 import 'package:evoluton_x/core/utils/app_routes.dart';
 import 'package:evoluton_x/core/utils/app_strings.dart';
 import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/core/utils/enums.dart';
 import 'package:evoluton_x/core/widgets/app_button.dart';
 import 'package:evoluton_x/features/authentication/presentation/controllers/login_bloc/login_bloc.dart';
 import 'package:evoluton_x/features/authentication/presentation/controllers/login_bloc/login_event.dart';
@@ -48,6 +54,8 @@ class LoginForm extends StatelessWidget {
           ),
           const SizedBox(height: 8),
           BlocBuilder<LoginBloc, LoginState>(
+            buildWhen: (previous, current) =>
+                previous.isObscurePass != current.isObscurePass,
             builder: (context, state) {
               return CustomTextFormField(
                 controller: bloc.passwordController,
@@ -59,33 +67,64 @@ class LoginForm extends StatelessWidget {
                     ? AppIconAssets.visibilityOff
                     : AppIconAssets.visibility,
                 onSuffix: () => bloc.add(TogglePasswordVisibilityEvent()),
-                validator: (value) =>
-                    validateInput(val: value!, min: 6, type: 'password'),
+                validator: (value) => validateInput(
+                    val: value!, min: 8, max: 25, type: 'password'),
                 textInputAction: TextInputAction.done,
               );
             },
           ),
           const CustomForgetPassButton(),
           const SizedBox(height: 60),
-          BlocBuilder<LoginBloc, LoginState>(
+          BlocConsumer<LoginBloc, LoginState>(
+            listener: (context, state) {
+              if (state.loginState == RequestStates.failureState) {
+                showCustomErrSnackBar(
+                  context: context,
+                  errMessage: state.loginErrMessage,
+                );
+                bloc.add(ResetLoginStateEvent());
+              }
+              if (state.loginState == RequestStates.successState) {
+                _handleLoginSuccess(state, context);
+              }
+            },
+            buildWhen: (previous, current) =>
+                previous.loginState != current.loginState,
             builder: (context, state) {
-              return AppButton(
-                textButton: AppStrings.login,
-                widthButton: double.infinity,
-                onPressed: () {
-                  if (key.currentState!.validate()) {
-                    bloc.add(UserLoginEvent());
-                    Navigator.pushReplacementNamed(
-                      context,
-                      AppRoutes.homeAfterChecked,
-                    );
-                  }
-                },
-              );
+              if (state.loginState == RequestStates.loadingState) {
+                return const Center(child: CircularProgressIndicator());
+              } else {
+                return AppButton(
+                  textButton: AppStrings.login,
+                  widthButton: double.infinity,
+                  onPressed: () {
+                    if (key.currentState!.validate()) {
+                      bloc.add(UserLoginEvent());
+                    }
+                  },
+                );
+              }
             },
           ),
         ],
       ),
     );
   }
+
+  void _handleLoginSuccess(LoginState state, BuildContext context) {
+    getIt<CachServices>().saveData(
+      key: AppConstants.token,
+      value: state.loginAuthResponse!.data,
+    );
+    showCustomSuccessSnackBar(
+        context: context, successMessage: state.loginAuthResponse!.message);
+
+    Navigator.pushReplacementNamed(
+      context,
+      AppRoutes.homeAfterChecked,
+    );
+  }
 }
+//7|aToV2hHwk8iy23KdsOlOTCfJfMQwt7pJwzOFI9fR7428174a
+
+
