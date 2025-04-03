eabab75791fbc6549b40a0352e2c3a5e65d6b816
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-03 13:21:42 +0300

    Refactoring code.

--- lib/core/functions/show_custom_err_snack_bar.dart
+++ lib/core/functions/show_custom_err_snack_bar.dart
@@ -1,12 +1,15 @@
 import 'package:evoluton_x/core/widgets/custom_err_message.dart';
 import 'package:flutter/material.dart';
 
-SnackBar showCustomErrSnackBar({required String errMessage}) {
-  return SnackBar(
-    content: CustomErrorMessage(errMessage: errMessage),
-    behavior: SnackBarBehavior.fixed,
-    duration: const Duration(seconds: 3),
-    backgroundColor: Colors.transparent,
-    elevation: 0,
+void showCustomErrSnackBar(
+    {required BuildContext context, required String errMessage}) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      content: CustomErrorMessage(errMessage: errMessage),
+      behavior: SnackBarBehavior.fixed,
+      duration: const Duration(seconds: 3),
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
   );
 }
