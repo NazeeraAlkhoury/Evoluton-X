a44d5df243dbfce82606fde22cc2187c1e135c64
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-04 15:31:19 +0300

    Create ResendCodeSection.

---
+++ lib/features/authentication/presentation/widgets/verify_password/resend_code_section.dart
@@ -0,0 +1,41 @@
+import 'package:evoluton_x/core/functions/show_custom_err_snack_bar.dart';
+import 'package:evoluton_x/core/functions/show_custom_success_snack_bar.dart';
+import 'package:evoluton_x/core/utils/enums.dart';
+import 'package:evoluton_x/features/authentication/presentation/controllers/password_bloc/password_bloc.dart';
+import 'package:evoluton_x/features/authentication/presentation/controllers/password_bloc/password_event.dart';
+import 'package:evoluton_x/features/authentication/presentation/controllers/password_bloc/password_state.dart';
+import 'package:evoluton_x/features/authentication/presentation/widgets/resend_code_row.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ResendCodeSection extends StatelessWidget {
+  const ResendCodeSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    PasswordBloc bloc = context.read<PasswordBloc>();
+    return BlocConsumer<PasswordBloc, PasswordState>(
+      listener: (context, state) {
+        if (state.forgetPassState == RequestStates.failureState) {
+          showCustomErrSnackBar(
+              context: context, errMessage: state.forgetPassErrMessage);
+        }
+        if (state.forgetPassState == RequestStates.successState) {
+          showCustomSuccessSnackBar(
+            context: context,
+            successMessage: state.forgetPAssAuthResponse!.message,
+          );
+        }
+      },
+      buildWhen: (previous, current) =>
+          previous.forgetPassState != current.forgetPassState,
+      builder: (context, state) {
+        return ResndCodeRow(
+          onPressed: () => bloc.add(ForgetPasswordEvent()),
+        );
+      },
+    );
+  }
+}
