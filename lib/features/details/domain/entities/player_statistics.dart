71c38c3e0ac014048020c86433cab52c70733c6b
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-06 13:58:28 +0300

    Create PlayerStatistics && PlayerDetails.

---
+++ lib/features/details/domain/entities/player_statistics.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:evoluton_x/features/details/domain/entities/player_statistics_card.dart';
+
+class PlayerStatistics extends Equatable {
+  final String msg;
+  final PlayerStatisticsCard data;
+
+  const PlayerStatistics({required this.msg, required this.data});
+
+  @override
+  List<Object?> get props => [
+        msg,
+        data,
+      ];
+}
