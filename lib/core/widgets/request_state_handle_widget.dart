12ed9e6d1c21b474f993f64ad406d08c11e27f36
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2025-04-06 12:23:12 +0300

    Create RequestStatesHandleWidget.

---
+++ lib/core/widgets/request_state_handle_widget.dart
@@ -0,0 +1,53 @@
+import 'package:evoluton_x/core/utils/app_colors.dart';
+import 'package:evoluton_x/core/utils/app_text_styles.dart';
+import 'package:evoluton_x/core/utils/enums.dart';
+import 'package:flutter/material.dart';
+
+class RequestStateHandleWidget extends StatelessWidget {
+  final RequestStates requestState;
+  final String errorMessage;
+  final WidgetBuilder successWidget;
+
+  const RequestStateHandleWidget(
+      {super.key,
+      required this.requestState,
+      required this.errorMessage,
+      required this.successWidget});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (requestState) {
+      case RequestStates.failureState:
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.error_outline,
+                color: AppColors.blackColor,
+                size: 24,
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Text(
+                  errorMessage,
+                  style: AppTextStyles.styleSemiBold16(context),
+                ),
+              ),
+            ],
+          ),
+        );
+      case RequestStates.loadingState:
+        return const SizedBox(
+          height: 155.8,
+          child: Center(child: CircularProgressIndicator()),
+        );
+      case RequestStates.successState:
+        return successWidget(context);
+
+      default:
+        return const SizedBox();
+    }
+  }
+}
